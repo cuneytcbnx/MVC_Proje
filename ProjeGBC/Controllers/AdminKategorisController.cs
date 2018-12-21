a6966b0c5fbe3928f1908fd105938628a7cff1b9
Cuneytcbnx <cuneytcbnx@gmail.com>
2018-12-22 00:28:55 +0300

    Proje dosyası ekle.

---
+++ ProjeGBC/Controllers/AdminKategorisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjeGBC.Models;
+
+namespace ProjeGBC.Controllers
+{
+    public class AdminKategorisController : Controller
+    {
+        private mvcblogDB db = new mvcblogDB();
+
+        // GET: AdminKategoris
+        public ActionResult Index()
+        {
+            return View(db.Kategoris.ToList());
+        }
+
+        // GET: AdminKategoris/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategoris.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // GET: AdminKategoris/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminKategoris/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KategoriId,KategoriAdi")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategoris.Add(kategori);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(kategori);
+        }
+
+        // GET: AdminKategoris/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategoris.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // POST: AdminKategoris/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KategoriId,KategoriAdi")] Kategori kategori)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(kategori).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(kategori);
+        }
+
+        // GET: AdminKategoris/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Kategori kategori = db.Kategoris.Find(id);
+            if (kategori == null)
+            {
+                return HttpNotFound();
+            }
+            return View(kategori);
+        }
+
+        // POST: AdminKategoris/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Kategori kategori = db.Kategoris.Find(id);
+            db.Kategoris.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
